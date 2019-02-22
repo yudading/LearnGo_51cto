e9809a45805caf4c6bac34b97f5129e987c56115
yudading <439219664@qq.com>
2019-02-22 18:17:47 +0800

    1 aa 别名
    // init 函数 go 执行时自动运行框架调用
    2 func init(){
    
    }

---
+++ LearnGoProject/day1/add/addgo.go
@@ -0,0 +1,15 @@
+package addgopackage
+
+var Name string = "hello world"
+var Age int = 16
+
+func init() {
+	Name = "hello go"
+	Age = 101
+}
+
+/*func Test(){
+	Name = "hello go"
+	Age = 100
+}
+*/
---
+++ LearnGoProject/day1/useadd/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	aa "../add"
+	"fmt"
+)
+
+func main() {
+	//aa.Test()
+	fmt.Println(aa.Name)
+	fmt.Println(aa.Age)
+}
