2a31c761cc46f57d7f1ace127396437c3cc757ff
yudading <439219664@qq.com>
2019-03-20 10:32:03 +0800

    day2 example3
    result string
    
    var result string = a
    
    return result

--- LearnGoProject/day2/example3/main.go
+++ LearnGoProject/day2/example3/main.go
@@ -10,7 +10,17 @@ func add(a int, arg ...int) int {
 	return sum
 }
 
+func concat(a string, arg ...string) string {
+	var result string = a
+	for i := 0; i < len(arg); i++ {
+		result += arg[i]
+	}
+	return result
+}
 func main() {
 	sum := add(10, 2, 3, 4, 5)
 	fmt.Println(sum)
+
+	res := concat("hello", " ", "world")
+	fmt.Println(res)
 }
