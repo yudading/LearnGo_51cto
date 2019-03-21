739502ed0c9cc592220644cce018552ea5a5b45e
yudading <439219664@qq.com>
2019-03-21 10:22:51 +0800

    day2 example4
    p *int
    
    *p = 100
    
    &a

--- LearnGoProject/day2/example4/main.go
+++ LearnGoProject/day2/example4/main.go
@@ -10,4 +10,18 @@ func main() {
 	var a int = 10
 	modify(&a)
 	fmt.Println(a)
+
+	var p *int
+	p = &a
+	fmt.Println(*p)
+
+	*p = 1000
+	fmt.Println(a)
+
+	var b int = 999
+	p = &b
+	*p = 5
+
+	fmt.Println(a)
+	fmt.Println(b)
 }
